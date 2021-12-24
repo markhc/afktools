7753dda14fbdaefd164eeca4a6f65a7276b95da8
markhc <mconceicao@ntstatus.net>
2021-12-24 18:05:00 -0300

    Added base path

--- restapi/src/index.ts
+++ restapi/src/index.ts
@@ -20,6 +20,7 @@ if (require.main === module) {
   // Run the application
   const config = {
     rest: {
+      basePath: '/api',
       port: +(process.env.PORT ?? 3000),
       host: process.env.HOST,
       // The `gracePeriodForClose` provides a graceful close for http/https
